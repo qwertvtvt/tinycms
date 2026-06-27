ac905e299fbe687e32fed21d7bd93d507e8869bc
qwertvtvt <mizuki20100112@outlook.jp>
2026-06-28 01:29:18 +0900

    update

--- api/post_article.php
+++ api/post_article.php
@@ -32,7 +32,6 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
     $id = generateId(5);
     $title = htmlspecialchars($_POST["title"], ENT_QUOTES, "UTF-8");
     $content = htmlspecialchars($_POST["content"], ENT_QUOTES, "UTF-8");
-    $content = nl2br($content);
     $post_at = (int) floor(microtime(true)  * 1000);
     $images = [];
 
